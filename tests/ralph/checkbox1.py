79a40145c7aef25a9003a6d7a3c972b38fc4eff7
ralph van der horst <ralphvanderhorst@learnautomatedtesting.com>
2024-11-03 13:16:13 +0000

    Add or update generated Playwright test script

--- tests/ralph/checkbox1.py
+++ tests/ralph/checkbox1.py
@@ -1,25 +1,22 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import pytest
 
 @pytest.fixture
-def setup():
+def driver():
     driver = webdriver.Firefox()
-    driver.implicitly_wait(15)
     yield driver
-    driver.quit()
+    driver.quit() 
 
-def test_checkboxes(setup):
-    driver = setup
+def test_checkboxes(driver):
     driver.get("https://practiceautomatedtesting.com/webelements/Checkboxes")
-
-    labels = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.checkbox-label ')))
     
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'body')))
+
+    labels = driver.find_elements_by_class_name('checkbox-label ')
     for label in labels:
-        checkbox_class_before_click = label.find_element_by_xpath('./span').get_attribute("class")
         label.click()
-        checkbox_class_after_click = label.find_element_by_xpath('./span').get_attribute("class")
-        
-        assert checkbox_class_before_click != checkbox_class_after_click
+        span = label.find_element_by_tag_name('span')
+        assert ('smiley' in span.get_attribute('class') or 'bad smiley' in span.get_attribute('class')), f"Checkbox {label.text} did not display the correct icon."
