7f8c7b3e95c73bacabc26d7f823e6e761722aa7a
ralph van der horst <ralphvanderhorst@learnautomatedtesting.com>
2024-11-03 12:58:00 +0000

    Add or update generated Playwright test script

--- tests/ralph/checkbox.py
+++ tests/ralph/checkbox.py
@@ -3,25 +3,29 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
 
-def test_checkbox_interaction():
-    driver = webdriver.Firefox()
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
+
+def test_checkboxes(driver):
     driver.get('https://practiceautomatedtesting.com/webelements/Checkboxes')
 
-    checkbox1 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'checkbox1')))
-    checkbox2 = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'checkbox2')))
+    checkbox1 = driver.find_element_by_id('checkbox1')
+    checkbox1.click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'checkbox1')))
+    assert checkbox1.is_selected(), "Checkbox1 not activated!"
 
-    ActionChains(driver).click(checkbox1).perform()
+    checkbox2 = driver.find_element_by_id('checkbox2')
+    checkbox2.click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,'checkbox2')))
+    assert checkbox2.is_selected(), "Checkbox2 not activated!"
+    
+    smiley = driver.find_element_by_class_name('smiley')
+    assert smiley.is_displayed(), "Smiley is not displayed!"
 
-    # Replace 'smiley' with the actual id, class or xpath of the smiley element
-    smiley1 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'smiley')))
-    assert smiley1.is_displayed()
-
-    ActionChains(driver).click(checkbox2).perform()
-
-    # Replace 'badsmiley' with the actual id, class or xpath of the bad smiley 
-    bad_smiley2 = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'badsmiley')))
-    assert bad_smiley2.is_displayed()
-
-    driver.quit()
+    bad_smiley = driver.find_element_by_class_name('bad_smiley')
+    assert not bad_smiley.is_displayed(), "Bad smiley is displayed!"
